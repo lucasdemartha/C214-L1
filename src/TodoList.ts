2d375090f91c030394ac33f01229841b52741b7e
lucasdemartha <lucasribm@gmail.com>
2024-03-15 10:05:07 -0300

    feat: validate task before insert

--- src/TodoList.ts
+++ src/TodoList.ts
@@ -1,27 +1,50 @@
-//title
-//description
-//targetDate
-//type
-//priority
-//subTasks
-
 export type Task = {
     title: string,
     description: string,
     targetDate: string,
     type?: string,
     priority?: string,
-    subTasks?: Task[]
+    subTasks?: Task []
+}
+
+export type UpdateTask = {
+  title?: string,
+  description?: string,
+  targetDate?: string,
+  type?: string,
+  priority?: string,
+  subTasks?: Task []
 }
 
 export class ToDoList {
-    private tasks: Task[] = []
+  private tasks: Task[] = []
 
-    add (task: Task) {
-        this.tasks.push(task)
+  add (task: Task) {
+    const missingProperties = ['title', 'description', 'targetDate'].filter(
+      (prop) => !Object.keys(task).includes(prop)
+    )
+    try {
+      if (missingProperties.length > 0) {
+        return 'Missing properties in task object'
+      }
+      this.tasks.push(task)
+    } catch (error) {
+      return error
     }
+  }
 
-    getTasks () {
-        return this.tasks
+  getTasks () {
+    return this.tasks
+  }
+
+  updateTask (index: number, task: UpdateTask) {
+    this.tasks[index] = {
+      ...this.tasks[index],
+      ...task
     }
+  }
+
+  removeTask (index: number) {
+    this.tasks.splice(index, 1)
+  }
 }
